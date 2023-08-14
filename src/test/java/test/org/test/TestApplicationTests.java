52d5144b75b563663396d1999a2c9f0fcccafc57
BlueOceanDolphin <ehfrhfo313@naver.com>
2023-08-14 09:53:00 +0900

    초기 설정

---
+++ src/test/java/test/org/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package test.org.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
